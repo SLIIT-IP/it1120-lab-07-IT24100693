61a46d18b69610ccd1932cb93c42b7721c20db88
IT24100693 <it24100693@my.sliit.lk>
2024-10-02 07:30:53 +0530

    Add files via upload

---
+++ Q2/IT24100693Lab7Q2A.java
@@ -0,0 +1,14 @@
+public class IT24100693Lab7Q2A {
+    public static void main(String[] args) {
+        int i = 0;
+        while (i < 5) {
+            int j = 0;
+            while (j < 5) {
+                System.out.print("$ ");
+                j++;
+            }
+            System.out.println();
+            i++;
+        }
+    }
+}
